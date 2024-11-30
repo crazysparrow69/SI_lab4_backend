05d1dcc1286ed72751bbc8fc043df3bbb56f8fe5
crazysparrow69 <denissytnikmacho@gmail.com>
2024-11-30 19:14:19 +0200

    feat: cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
   const configService = app.get(ConfigService);
   await app.listen(configService.get<number>("PORT") ?? 5000);
 }
